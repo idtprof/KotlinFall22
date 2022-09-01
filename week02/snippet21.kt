ade79c2b6dd6b2d5b2599d7fcaf8973f56ca2f8c
Francisco I. Rodriguez <firodriguez@dcccd.edu>
2022-09-01 09:06:52 -0500

    Create snippet21.kt

---
+++ week02/snippet21.kt
@@ -0,0 +1,13 @@
+Code for 02.1
+
+	var initial: Char = 'F'
+    println(initial)
+    
+    	var initial: Char = 'F'
+    println(initial)
+    
+    	var initial: Char = 'F'
+    println(initial)
+    
+    	var initial: Char = 'F'
+    println(initial)
